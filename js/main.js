40159eb7d336b31f81176a1da3b11b082e6dac21
brackenBYUI <don18004@byui.edu>
2021-11-20 18:45:42 -0700

    Week 10 notes

--- js/main.js
+++ js/main.js
@@ -62,6 +62,14 @@ const links = [
     {
       label: "Week 9 Team Activity",
       url: "week9/teamActivity/index-START.html"
+    },
+    {
+      label: "Week 10 Notes",
+      url: "week10/index.html"
+    },
+    {
+      label: "Week 10 Team Activity",
+      url: "week10/teamActivity/index.html"
     }
   ]
 
